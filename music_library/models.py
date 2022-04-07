eaced49edabdfe0faf6a5fae4cc81b6460bfd988
itachi363 <itachi363@yahoo.com>
2022-04-07 12:45:00 -0400

    made music model in models

--- music_library/models.py
+++ music_library/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.forms import CharField
+from datetime import datetime, date
 
 # Create your models here.
+
+class Music(models.Model):
+    title = models.CharField(max_length=255)
+    artist = models.CharField(max_length=255)
+    album = models.CharField(max_length=255)
+    release_date = models.DateField(auto_now_add=False, auto_now=False, blank=True)
+    genre = models.CharField(max_length=255)
