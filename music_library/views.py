bda2bea790be141ccaaf6e6d6309a13fc3a6f124
itachi363 <itachi363@yahoo.com>
2022-04-07 16:12:18 -0400

    updated views with get, put and delete

--- music_library/views.py
+++ music_library/views.py
@@ -1,9 +1,11 @@
+from django.shortcuts import get_object_or_404
 from urllib import response
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import MusicSerializer
 from .models import Music
+from music_library import serializers
 
 @api_view(['GET', 'POST'])
 def music_list(request):
@@ -19,3 +21,17 @@ def music_list(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+@api_view(['GET', 'PUT', 'DELETE'])
+def music_detail(request, pk):
+    music = get_object_or_404(Music, pk=pk)
+    if request.method == 'GET':
+        serializer = MusicSerializer(music)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'PUT':
+        serializer = MusicSerializer(music, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        music.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
